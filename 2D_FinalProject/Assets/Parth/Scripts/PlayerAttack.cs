8cc1305e8a88c44112dedc4cc7ddb9aa7305d73a
ParthPoojara <47029174+ParthPoojara@users.noreply.github.com>
2019-11-12 17:03:27 +0000

    PlayerAttack
    Added player attack animation. now when the player press the "f" button then the player can attack and it will play the correlated animation.

---
+++ 2D_FinalProject/Assets/Parth/Scripts/PlayerAttack.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private bool attacking = false;
+
+    private float attacktimer = 0;
+    private float attackcool = 0.3f;
+
+    public Collider2D attacktrigger;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        attacktrigger.enabled = false; 
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("f") && !attacking)
+        {
+            attacking = true;
+            attacktimer = attackcool;
+            attacktrigger.enabled = true;
+        }
+       
+        if (attacking)
+        {
+            if(attacktimer > 0)
+            {
+                attacktimer -= Time.deltaTime;
+            }
+            else
+            {
+                attacking = false;
+                attacktrigger.enabled = false;
+            }
+        }
+        anim.SetBool("IsAttacking", attacking);
+        
+
+    }
+
+
+}
