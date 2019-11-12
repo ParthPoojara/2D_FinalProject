2d92aec719feaf907caf2dcc2f7a78bf1111788c
ParthPoojara <47029174+ParthPoojara@users.noreply.github.com>
2019-11-12 19:04:51 +0000

    Menus & Animation
    Added Main menu with functional button. added death animation to the player when the player loses heatlh.

---
+++ 2D_FinalProject/Assets/Parth/Scripts/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerHealth : MonoBehaviour 
+{
+	[SerializeField]
+	Slider healthBar;
+
+	[SerializeField]
+	Text healthText;
+
+	[SerializeField]
+	//GameObject DeathUI;
+
+	Animator myAnimator;
+
+	float maxHealth = 100;
+	float curHealth;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		myAnimator = GetComponent<Animator> ();
+		myAnimator.enabled = true;
+
+		healthBar.value = maxHealth;
+		curHealth = healthBar.value;
+	}
+
+	void OnTriggerStay2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Saw") 
+		{
+			healthBar.value -= 1.00f;
+			curHealth = healthBar.value;
+		}
+	}
+
+	void Update()
+	{
+		healthText.text = curHealth.ToString () + " %";
+
+		if (curHealth <= 0) 
+		{
+			myAnimator.SetBool ("IsDead", true);
+            Destroy(gameObject, 3f);
+		
+			//DeathUI.gameObject.SetActive (true);
+		}
+
+	
+	}
+		
+}
