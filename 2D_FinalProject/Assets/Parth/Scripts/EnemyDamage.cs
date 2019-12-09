33277babdcca07ee9d93a2f85d146dbd403c71d9
ParthPoojara <47029174+ParthPoojara@users.noreply.github.com>
2019-12-09 14:27:26 +0000

    Combo Attack Animation
    wrote a script that allows the players to combo attack or single attack by pressing the mouse button. one mouse button is single attack, if you click the right mouse button twice it does the second attack and if you press it three times it does its final attack.

---
+++ 2D_FinalProject/Assets/Parth/Scripts/EnemyDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public float health;
+    private mydamage damagescript;
+    public GameObject hitbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damagescript = hitbox.GetComponent<mydamage>();
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == hitbox.tag)
+        {
+            health -= damagescript.realDamage;
+        }
+    }
+
+}
--- 2D_FinalProject/Assets/Parth/Scripts/mydamage.cs
+++ 2D_FinalProject/Assets/Parth/Scripts/mydamage.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackTrigger : MonoBehaviour
+public class mydamage : MonoBehaviour
 {
-   
+    public int realDamage;
 }
-
