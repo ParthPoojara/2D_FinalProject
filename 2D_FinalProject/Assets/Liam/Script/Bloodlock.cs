4ac9d182374c233fbeebc8c420ba2804d459f045
HitoriMusuko <Hitori_Musuko@users.noreply.github.com>
2020-02-24 15:51:40 +0000

    Bloodlock and Scene Transition
    Added bloodlock and scene transition

---
+++ 2D_FinalProject/Assets/Liam/Script/Bloodlock.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloodlock : MonoBehaviour
+{
+
+    public GameObject[] GOcollectibles;
+    public GameObject TriggeredObject;
+    public bool enabled;
+        
+        // Start is called before the first frame update
+    void Start()
+    {
+        // Disables the target object
+        if (enabled) TriggeredObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // number of objects started with
+        int ObjectsLeft = GOcollectibles.Length;
+
+        // loop through activation objects
+        foreach (GameObject GO in GOcollectibles)
+        {
+            //Take away an object when destoryed
+            if (!GO) ObjectsLeft--;
+        }
+
+        // There are no objects left
+        if (ObjectsLeft < 1)
+        {
+            // Enabling and disabling objects
+            if (enabled)
+                TriggeredObject.SetActive(true);
+            else
+                TriggeredObject.SetActive(false);
+        }
+    }
+}
