aa95236331f388db72e51d6d8a59a5a8f3040d5c
ParthPoojara <47029174+ParthPoojara@users.noreply.github.com>
2020-03-15 12:57:00 +0000

    Dialogue Update
    Tried using a new dialogue system for the game and added new panels and text for the dialog box. added new piece of code for the dialog box but debugging still has issues and dialog box still not displaying on trigger.

---
+++ 2D_FinalProject/Assets/Parth/Scripts/npcdialogue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Npcdialogue 
+{
+    public string nameofperson;
+
+    [TextArea(3, 10)]
+    public string[] sentencelist;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D_FinalProject/Assets/Parth/Scripts/npcdialoguemanager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class npcdialoguemanager : MonoBehaviour
+{
+    public Npcdialogue dialogue;
+
+    Queue<string> sentences;
+
+    public GameObject dialogpanel;
+
+    public TextMeshProUGUI displaytext;
+
+    string activesentence;
+    public float tyingspeed;
+
+    AudioSource myaudio;
+    public AudioClip speaksound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        myaudio = GetComponent<AudioSource>();
+    }
+
+    void StartDialogue()
+    {
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentencelist)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    void DisplayNextSentence()
+    {
+        if(sentences.Count <= 0)
+        {
+            displaytext.text = activesentence;
+            return;
+        }
+
+        activesentence = sentences.Dequeue();
+        displaytext.text = activesentence;
+
+        StopAllCoroutines();
+        StartCoroutine(TypeTheSentence(activesentence));
+
+    }
+
+    IEnumerator TypeTheSentence(string sentence)
+    {
+        displaytext.text = "";
+
+        foreach(char letter in sentence.ToCharArray())
+        {
+            displaytext.text += letter;
+            myaudio.PlayOneShot(speaksound);
+            yield return new WaitForSeconds(tyingspeed);
+        }
+    }
+    
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            dialogpanel.SetActive(true);
+
+            StartDialogue();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.F) && displaytext.text == activesentence)
+            {
+                DisplayNextSentence();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            dialogpanel.SetActive(false);
+            StopAllCoroutines();
+        }
+    }
+
+}
