1d0fe55f0b4692b5c25874211eea5bab570590ea
HitoriMusuko <44027061+HitoriMusuko@users.noreply.github.com>
2020-03-16 00:38:46 +0000

    Basic enemy movement
    Made a basic enemy movment system, will work more soon

--- 2D_FinalProject/Assets/Liam/Script/EnemyPatrol.cs
+++ 2D_FinalProject/Assets/Liam/Script/EnemyPatrol.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyPatrol : MonoBehaviour
 {
+    public float speed;
+
+    public bool MoveRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (MoveRight)
+        {
+            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
+            
+        }
+        else
+        {
+            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
+            
+        }
+    }
+    void OnTriggerEnter2D(Collider2D PatrolPointColl)
+    {
+        if (PatrolPointColl.gameObject.CompareTag("Patrol"))
+        {
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
+        }
     }
 }
