64db908db40a58f9d542565eb5e9b17a6e205a29
fly1ch <47030065+fly1ch@users.noreply.github.com>
2020-03-29 18:13:43 +0100

    Fungus Dialogue
    Added fungus with a dialogue script

---
+++ 2D_FinalProject/Assets/Chris/Scripts/FungusProximity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class FungusProximity: MonoBehaviour
+{
+    public GameObject GO_thePlayer;
+
+    public float fl_minTriggerDistance;
+
+    public string st_fungusMessage = "TestMessage";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDistance = Vector3.Distance(this.transform.position, GO_thePlayer.transform.position);
+
+        if (currentDistance <= fl_minTriggerDistance)
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+                Fungus.Flowchart.BroadcastFungusMessage(st_fungusMessage);
+            }
+        }
+    }
+}
