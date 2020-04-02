e9b175cd34e2f65eef1d14a676d15e41556047b7
ParthPoojara <47029174+ParthPoojara@users.noreply.github.com>
2020-04-02 22:52:40 +0100

    Health Potion Effect
    Added particle effects to the health potions to make it more immersive.

--- 2D_FinalProject/Assets/Parth/Scripts/HealthPickup.cs
+++ 2D_FinalProject/Assets/Parth/Scripts/HealthPickup.cs
@@ -1,24 +1,48 @@
 ﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class HealthPickup : MonoBehaviour
 {
     PlayerHealth playerhealth;
+
+    private SpriteRenderer sr;
+    private ParticleSystem particle;
+    private BoxCollider2D bc;
+
 
     public float healthbonus = 15f;
 
     void Awake()
     {
         playerhealth = FindObjectOfType<PlayerHealth>();
+        particle = GetComponentInChildren<ParticleSystem>();
+        sr = GetComponent<SpriteRenderer>();
+        bc = GetComponent<BoxCollider2D>();
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if(playerhealth.curHealth < playerhealth.maxHealth)
         {
-            Destroy(gameObject);
+            
             playerhealth.curHealth = playerhealth.curHealth + healthbonus;
+            StartCoroutine(Break());
         }
     }
 
+    private IEnumerator Break()
+    {
+        particle.Play();
+
+        sr.enabled = false;
+        bc.enabled = false;
+
+        yield return new WaitForSeconds(particle.main.startLifetime.constantMax);
+
+        Destroy(gameObject);
+
+    }
 
 }
