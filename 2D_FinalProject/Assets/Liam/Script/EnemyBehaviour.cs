2223f3e1a393e2b5881bfb22b9d69275ac5646e6
HitoriMusuko <44027061+HitoriMusuko@users.noreply.github.com>
2020-04-03 21:40:16 +0100

    Enemy Trigger area
    Created an enemy trigger area which will activate the animations and attacks when entered and follow the player.

---
+++ 2D_FinalProject/Assets/Liam/Script/EnemyBehaviour.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    #region Public Variables (Variables viewable in the inspector)
+    public Transform rayCast; // The location of the raycast
+    public LayerMask raycastMask; // The layer which the racast is placed on
+    public float rayCastLength; // Length of the raycast
+    public float attackDistance; // Minimum attack range
+    public float moveSpeed; // The movement speed of the enemy NPC
+    public float timer; // Cooldown timer between attacks
+    #endregion
+
+    #region Private Variables (Variables not viewable in the inspector)
+    private RaycastHit2D hit; // Telling if the raycast hits
+    private GameObject target; // The enemy's target
+    private Animator anim; // Links the animator
+    private float distance; // The distance between enemy and player
+    private bool attackMode; // Note whether the enemy is attacking or not
+    private bool inRange; // See if the player is in range
+    private bool cooling; // Sees if the enemy is cooling down after an attack
+    private float intTimer; // Sets the initial timer 
+    #endregion
+
+    void Awake()
+    {
+        intTimer = timer; // Stores the initial value of timer
+        anim = GetComponent<Animator>(); // Accesses the amimator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // If the player is in range
+        if (inRange)
+        {
+            // Checks for a hit with a raycast and stores the information
+            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            // Test the Raycast is working correctly and allows it to be seen
+            RaycastDebugger();
+        }
+
+        // When the player is detected
+        // If the hit collider is not equal to null
+        if(hit.collider != null)
+        {
+            EnemyLogic();
+        }
+        // If the hit collider is equal to null
+        else if(hit.collider == null)
+        {
+            inRange = false;
+        }
+
+        // If the player isn't in range
+        if(inRange == false)
+        {
+            // Triggers the walking animation to false
+            anim.SetBool("canWalk", false);
+            StopAttack();
+        }
+    }
+
+    // Once the Gameobject (Player) enters the trigger area
+    void OnTriggerEnter(Collider trig) 
+    {
+        // If the object entering the trigger area is tagged player in the inspector then the condition is activated
+        if (trig.gameObject.tag == "Player") 
+        {
+            target = trig.gameObject; // The enemy will target the gameObject in the trigger area
+            inRange = true; // Indicates the player is in range
+        }
+    }
+
+    // Creating logic for the enemy
+    void EnemyLogic()
+    {
+        // This sets the distance between the player and enemy using the gameObject transform (enemy) and the target (player) transform 
+        distance = Vector2.Distance(transform.position, target.transform.position);
+
+        // If the distance is greater than the attack distance the condition is triggered
+        if (distance > attackDistance)
+        {
+            // Activates the move and stop attack function
+            Move();
+            StopAttack();
+        }
+        // If the attack distance is greater than or equal to the distance and the cooling is false
+        else if (attackDistance >= distance && cooling == false)
+        {
+            // Activate the attack function
+            Attack();
+        }
+
+        // If the enemy is cooling down
+        if (cooling)
+        {
+            // Calls the cooldown function
+            Cooldown();
+            // Sets the attack animation to false
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    // Set the move function
+    void Move()
+    {
+        // Sets the walking animation to true
+        anim.SetBool("canWalk", true);
+        // Check if it is playing the enemy attack function currently
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("EnemyAttack"))
+        {
+            // 
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            // 
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        }
+    }
+
+    // Set the attack function
+    void Attack()
+    {
+        // Reset the timer when Player enters attack range
+        timer = intTimer;
+        // To check if the enemy can still attack or not
+        attackMode = true;
+
+        // Sets the walking animation to false
+        anim.SetBool("canWalk", false);
+        // Sets the attack animation to true
+        anim.SetBool("Attack", true);
+    }
+
+    void Cooldown()
+    {
+        cooling = false;
+        timer = intTimer;
+    }
+
+    void StopAttack()
+    {
+        // Sets cooling to false
+        cooling = false;
+        // Sets attack mode to false
+        attackMode = false;
+        // Sets attack animation to false
+        anim.SetBool("Attack", false);
+    }
+
+    void RaycastDebugger()
+    {
+        // If the distance between the player and enemy is greater than the attack distance, the condition is triggered
+        if (distance > attackDistance)
+        {
+            // Creates a ray that is visible for us to see in a certain colour
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+        }
+        // If the attack distance is greater than the distance between the enemy and player, the condition is triggered
+        else if (attackDistance > distance)
+        {
+            // Creates a ray that is visible for us to see in a certain colour
+            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.magenta);
+        }
+        
+    }
+    // Sets the trigger cooling function
+    public void TriggerCooling()
+    {
+        cooling = true;
+    }
+}
