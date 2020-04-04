88d3f35a31492f77725300155865db9ec022941e
HitoriMusuko <44027061+HitoriMusuko@users.noreply.github.com>
2020-04-04 20:29:44 +0100

    Patrol
    Added patrol to the enemy script and changed some of the object positions and scale in the NPC test scene

--- 2D_FinalProject/Assets/Liam/Script/EnemyBehaviour.cs
+++ 2D_FinalProject/Assets/Liam/Script/EnemyBehaviour.cs
@@ -11,11 +11,13 @@ public class EnemyBehaviour : MonoBehaviour
     public float attackDistance; // Minimum attack range
     public float moveSpeed; // The movement speed of the enemy NPC
     public float timer; // Cooldown timer between attacks
+    public Transform LeftPatrol;
+    public Transform RightPatrol;
     #endregion
 
     #region Private Variables (Variables not viewable in the inspector)
     private RaycastHit2D hit; // Telling if the raycast hits
-    private GameObject target; // The enemy's target
+    private Transform target; // The enemy's target
     private Animator anim; // Links the animator
     private float distance; // The distance between enemy and player
     private bool attackMode; // Note whether the enemy is attacking or not
@@ -26,6 +28,7 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Awake()
     {
+        SelectTarget(); // Calls the select target function
         intTimer = timer; // Stores the initial value of timer
         anim = GetComponent<Animator>(); // Accesses the amimator
     }
@@ -33,11 +36,23 @@ public class EnemyBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If they player is not in attack more
+        if (!attackMode)
+        {
+            Move();
+        }
+
+        //
+        if(!InsideofLimits() && !inRange && !anim.GetCurrentAnimatorStateInfo(0).IsName("EnemyAttack"))
+        {
+            SelectTarget();
+        }
+
        // If the player is in range
         if (inRange)
         {
             // Checks for a hit with a raycast and stores the information
-            hit = Physics2D.Raycast(rayCast.position, Vector2.left, rayCastLength, raycastMask);
+            hit = Physics2D.Raycast(rayCast.position, transform.right, rayCastLength, raycastMask);
             // Test the Raycast is working correctly and allows it to be seen
             RaycastDebugger();
         }
@@ -58,7 +73,7 @@ public class EnemyBehaviour : MonoBehaviour
         if(inRange == false)
         {
             // Triggers the walking animation to false
-            anim.SetBool("canWalk", false);
+            //anim.SetBool("canWalk", false);
             StopAttack();
         }
     }
@@ -69,8 +84,9 @@ public class EnemyBehaviour : MonoBehaviour
         // If the object entering the trigger area is tagged player in the inspector then the condition is activated
         if (trig.gameObject.tag == "Player") 
         {
-            target = trig.gameObject; // The enemy will target the gameObject in the trigger area
+            target = trig.transform; // The enemy will target the gameObject in the trigger area
             inRange = true; // Indicates the player is in range
+            Flip(); // Calls the flip function
         }
     }
 
@@ -78,13 +94,13 @@ public class EnemyBehaviour : MonoBehaviour
     void EnemyLogic()
     {
         // This sets the distance between the player and enemy using the gameObject transform (enemy) and the target (player) transform 
-        distance = Vector2.Distance(transform.position, target.transform.position);
+        distance = Vector2.Distance(transform.position, target.position);
 
         // If the distance is greater than the attack distance the condition is triggered
         if (distance > attackDistance)
         {
             // Activates the move and stop attack function
-            Move();
+            
             StopAttack();
         }
         // If the attack distance is greater than or equal to the distance and the cooling is false
@@ -113,7 +129,7 @@ public class EnemyBehaviour : MonoBehaviour
         if (!anim.GetCurrentAnimatorStateInfo(0).IsName("EnemyAttack"))
         {
             // 
-            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            Vector2 targetPosition = new Vector2(target.position.x, transform.position.y);
             // 
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
 
@@ -156,13 +172,13 @@ public class EnemyBehaviour : MonoBehaviour
         if (distance > attackDistance)
         {
             // Creates a ray that is visible for us to see in a certain colour
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.red);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.red);
         }
         // If the attack distance is greater than the distance between the enemy and player, the condition is triggered
         else if (attackDistance > distance)
         {
             // Creates a ray that is visible for us to see in a certain colour
-            Debug.DrawRay(rayCast.position, Vector2.left * rayCastLength, Color.magenta);
+            Debug.DrawRay(rayCast.position, transform.right * rayCastLength, Color.magenta);
         }
         
     }
@@ -170,5 +186,44 @@ public class EnemyBehaviour : MonoBehaviour
     public void TriggerCooling()
     {
         cooling = true;
+    }
+
+    // Check to see if they Enemy NPC is within the boundaries
+    private bool InsideofLimits()
+    {
+        return transform.position.x > LeftPatrol.position.x && transform.position.x < RightPatrol.position.x;
+    }
+    
+    // 
+    private void SelectTarget()
+    {
+        float distanceToLeft = Vector2.Distance(transform.position, LeftPatrol.position);
+        float distanceToRight = Vector2.Distance(transform.position, RightPatrol.position);
+
+        if (distanceToLeft > distanceToRight)
+        {
+            target = LeftPatrol;
+        }
+        else
+        {
+            target = RightPatrol;
+        }
+
+        Flip();
+    }
+
+    private void Flip()
+    {
+        Vector3 rotation = transform.eulerAngles;
+        if(transform.position.x > target.position.x)
+        {
+            rotation.y = 180f;
+        }
+        else
+        {
+            rotation.y = 0f;
+        }
+
+        transform.eulerAngles = rotation;
     }
 }
