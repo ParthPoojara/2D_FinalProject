0b9592140742effdda2cae08c845bf9878169b80
HitoriMusuko <44027061+HitoriMusuko@users.noreply.github.com>
2020-04-04 20:55:20 +0100

    Damage and prefabs
    Made some asset prefabs and attempted a damage script

---
+++ 2D_FinalProject/Assets/EnemyAttackDamage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackDamage : MonoBehaviour
+{
+
+    public float damageSent;
+
+    private PlayerHealth pchealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pchealth = GetComponent<PlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D Ecollision)
+    {
+        if(Ecollision.gameObject.tag == "Enemydamage")
+        {
+            pchealth.curHealth -= damageSent; 
+        }
+    }
+}
